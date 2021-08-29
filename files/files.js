2fcd04ff9fbb489cd7b5667b0392c4dd75e7f7b8
AdiLoyaDahan <47718915+adiloya@users.noreply.github.com>
2021-08-29 13:26:25 +0300

    added new tabs
    1. added files page
    2. added coursed page
    3. added bolt color to the menu
    4. added links to menu

---
+++ files/files.js
@@ -0,0 +1,24 @@
+
+function  openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.visibility = "hidden";
+  }
+  
+  function  closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.visibility ="visible";
+  }
+
+  if (typeof jQuery == 'undefined') {
+
+    alert('JQuery is not defined');
+  }
+  $(document).ready(function() {
+    $(function() {
+      $("input:radio").click(function(){
+        $("div1").hide();
+        var div = "#blk-"+$(this).val();
+        $(div).show();
+    });
+   });
+});
