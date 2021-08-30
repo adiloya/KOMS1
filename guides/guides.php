5a457d4b0bc1aee7f4b2db7e66ce8eaabf9e0560
orsasson2 <orsasson2@gmail.com>
2021-08-30 13:12:42 +0300

    php

---
+++ guides/guides.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "koms";
+
+$firstName = $_POST['fname'];
+$lastName = $_POST['lname'];
+$id = $_POST['id'];
+$email = $_POST['email'];
+$address = $_POST['address'];
+$center = $_POST['center'];
+$prof = $_POST['prof'];
+
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+  echo "Connection successful";
+}
+
+
+$sql = "INSERT INTO guides (FirstName, LastName,id,email,address,center,prof)
+VALUES ('$firstName', '$lastName', '$id','$email','$address','$center','$prof')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+$conn->close();
+?>
