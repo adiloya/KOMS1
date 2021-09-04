c90eef52463377f6571c72c2923ffc8cda936589
orsasson2 <orsasson2@gmail.com>
2021-09-04 10:48:18 +0300

    working form

--- guides/guides.php
+++ guides/guides.php
@@ -15,14 +15,15 @@ if(isset($_POST['Submit'])){
 $teacher_firstname = $_POST['teacher_firstname'];
 $teacher_lastname = $_POST['teacher_lastname'];
 $teacher_id = $_POST['teacher_id'];
+$phone = $_POST['phone'];
 $email = $_POST['email'];
 $address = $_POST['address'];
 $center = $_POST['center'];
 $prof = $_POST['prof'];
 
-
-$sql = "INSERT INTO guides (FirstName, LastName,id,email,address,center,prof)
-VALUES ('$firstName', '$lastName', '$id','$email','$address','$center','$prof')";
+}
+$sql = "INSERT INTO guides (teacher_firstname, teacher_lastname,teacher_id,email,address,phone,center,prof)
+VALUES ('$teacher_firstname', '$teacher_lastname', '$teacher_id','$email','$address','$phone','$center','$prof')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
@@ -30,7 +31,7 @@ if ($conn->query($sql) === TRUE) {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-}
+
 
 $conn->close();
 ?>
