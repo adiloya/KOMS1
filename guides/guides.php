dacc31f621e4bfcbc789ac106df35aaa5f3d94a3
adiloya <adida@wix.com>
2021-09-19 15:10:14 +0300

    changing DB cofiguration

--- guides/guides.php
+++ guides/guides.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "koms";
+$username = "shakedha";
+$password = "AEHPqAazeLLZZ";
+$dbname = "shakedha_komsdb";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
